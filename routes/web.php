88dff7019082255130d2d4a4762aaf476e760864
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2024-10-11 10:51:31 +0700

    change home

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.auth.login');
+    return redirect('home');
 });
 
 //pengecekan Login dahulu baru bisa akses aplikasi
